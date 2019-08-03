08230e6768fa0ba02086cf35852d91dbf39d545f
Paul <pvcarroll@gmail.com>
2019-08-03 13:37:33 -0500

    navigation graph

--- app/src/main/java/com/example/pulseofaustin/LandingFragment.kt
+++ app/src/main/java/com/example/pulseofaustin/LandingFragment.kt
@@ -1,7 +1,25 @@
 package com.example.pulseofaustin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
 
 class LandingFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_landing, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.logInButton).setOnClickListener {
+            findNavController().navigate(R.id.action_landingFragment_to_loginFragment)
+        }
+    }
 }
--- app/src/main/java/com/example/pulseofaustin/LoginFragment.kt
+++ app/src/main/java/com/example/pulseofaustin/LoginFragment.kt
@@ -1,8 +1,18 @@
 package com.example.pulseofaustin
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class LoginFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
